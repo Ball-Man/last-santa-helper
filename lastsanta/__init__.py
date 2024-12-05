6a75e89bfcef4d95ad44a813d6262e5594956cab
Francesco <francesco.mistri98@gmail.com>
2024-12-05 22:35:54 +0100

    Basic working scene using pyglet

--- lastsanta/__init__.py
+++ lastsanta/__init__.py
@@ -1,5 +1,44 @@
 import desper
+import pyglet_desper as pdesper
+import pyglet
+from pyglet.window import Window
+from pyglet.sprite import Sprite
+
+# Setup main loop and window
+loop = pdesper.Loop()
+desper.default_loop = loop
+window = Window()
+loop.connect_window_events(window, 'on_draw', 'on_mouse_press')
+
+
+@desper.event_handler('on_draw')
+class CameraProcessor(desper.Processor):
+
+    def __init__(self):
+        self.window = next(iter(pyglet.app.windows))
+
+    def process(self, dt):
+        pass
+
+    def on_draw(self):
+        self.window.clear()
+        self.world.dispatch(pdesper.ON_CAMERA_DRAW_EVENT_NAME)
+
+
+def world_transformer(handle, world: desper.World):
+    world.add_processor(CameraProcessor())
+
+    world.create_entity(pdesper.Camera(pdesper.retrieve_batch(world)))
+
+    world.create_entity(Sprite(desper.resource_map['image/test'],
+                               batch=pdesper.retrieve_batch(world)))
 
 
 def main():
-    print('ok')
+    pdesper.resource_populator(desper.resource_map, trim_extensions=True)
+
+    handle = desper.WorldHandle()
+    handle.transform_functions.append(pdesper.init_graphics_transformer)
+    handle.transform_functions.append(world_transformer)
+    loop.switch(handle)
+    loop.loop()
