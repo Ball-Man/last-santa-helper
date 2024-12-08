02fca20efa77969e1c827ea06d93cabb5c24ae93
Francesco <francesco.mistri98@gmail.com>
2024-12-08 16:13:31 +0100

    Add physics resolution of rect to ax collision

--- lastsanta/physics.py
+++ lastsanta/physics.py
@@ -102,6 +102,69 @@ class VelocityProcessor(desper.Processor):
             transform.position += velocity * dt
 
 
+@dataclass
+class CollisionAxes:
+    """An axes collider.
+
+    Use ``index == 0`` for a vertical axis. ``index == 1`` for a
+    horizontal axis.
+    """
+    pos: SupportsFloat
+    index: int
+
+
+class RectangleToAxisProcessor(desper.Processor):
+    """Handle collisions between rectangles and lines.
+
+    This is a simplified simulation that let rectangles
+    (main dynamic entities) bounce on axis.
+    """
+
+    def process(self, dt):
+        axis_entities = self.world.get(CollisionAxes)
+
+        for dynamic_entity, velocity in self.world.get(Velocity):
+            if not self.world.has_component(dynamic_entity, CollisionRectangle):
+                continue
+            norm_velocity = velocity.normalize()
+
+            # Check collisions with all axis
+            reflection = Vec2()
+            for _, axes in axis_entities:
+                reflection += self._resolve(
+                    dt, dynamic_entity, self.world.get_component(dynamic_entity,
+                                                                 desper.Transform2D),
+                    velocity, norm_velocity, axes)
+
+            # Change velocity (bounce)
+            if reflection.x > 0.1:
+                velocity.x = -velocity.x
+
+            if reflection.y > 0.1:
+                velocity.y = -velocity.y
+
+    def _resolve(self, dt: float, dynamic_entity: int, dynamic_transform: desper.Transform2D,
+                 dynamic_velocity: Velocity, norm_velocity: Velocity, axes: CollisionAxes):
+        """Apply velocity and resolve collision with axes."""
+        old_pos = dynamic_transform.position
+        dynamic_transform.position += dynamic_velocity * dt
+        collided = False
+
+        # Adjust collision
+        while axis_collision(axes.pos, axes.index, desper.controller(dynamic_entity, self.world)):
+            collided = True
+            dynamic_transform.position -= norm_velocity
+
+        # If a collision happened, return a vector accordingly
+        reflection_axis = [0, 0]
+        reflection_axis[axes.index] = 1 * collided
+
+        # If no collision, reset position
+        dynamic_transform.position = old_pos
+
+        return Vec2(*reflection_axis)
+
+
 @desper.event_handler('on_mouse_game_motion')
 class PointCheckDebug(desper.Controller):
     """Debug: find and log all objects found."""
