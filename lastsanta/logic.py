bef8d052afa8e4e881e72a2b5cdba0c0f8c06f8c
Francesco <francesco.mistri98@gmail.com>
2024-12-08 20:00:20 +0100

    Add grabbing mechanic (no way to release it)

--- lastsanta/logic.py
+++ lastsanta/logic.py
@@ -11,25 +11,42 @@ class Item:
     """Represents an in game item."""
 
 
-@desper.event_handler('on_mouse_game_press')
+@desper.event_handler('on_mouse_game_press', 'on_mouse_game_motion')
 class ItemDragManager(desper.Controller):
     """When clicked, drag items."""
+    dragged: desper.Controller | None = None
+    offset: Vec2 = Vec2()
 
     def on_mouse_game_press(self, point: Vec2, buttons: int, mod):
         """Event: handle mouse."""
         if pyglet.window.mouse.LEFT & buttons:
             self.begin_drag(point)
 
+    def on_mouse_game_motion(self, mouse_position: Vec2):
+        """If something is being dragged, drag it."""
+        if self.dragged is None:        # Nothing to drag
+            return
+
+        transform = self.dragged.get_component(desper.Transform2D)
+        transform.position = desper.math.Vec2(*(mouse_position - self.offset))
+
     def begin_drag(self, point: Vec2):
         """On mouse press, find intersecting items and grab one."""
+        # TODO: pixel perfect check
         # Find top item
-        top_item = -1
+        top_item = None
         top_order = -1
         for item_entity, _ in self.world.get(Item):
             item_controller = desper.controller(item_entity, self.world)
             sprite: Sprite = item_controller.get_component(Sprite)
             if physics.point_collision(point, item_controller) and top_order < sprite.group.order:
-                top_item = item_entity
+                top_item = item_controller
                 top_order = sprite.group.order
 
-        print('top item is', top_item)
+        if top_item is None:            # Nothing to drag
+            return
+
+        # Save current item and mouse offset for dragging
+        self.dragged = top_item
+        self.offset = point - top_item.get_component(desper.Transform2D).position
+        top_item.remove_component(physics.Velocity)
