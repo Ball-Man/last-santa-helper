ec71d8615e6699ef3557cabaed8c54a51c0b2b15
Francesco <francesco.mistri98@gmail.com>
2024-12-19 20:58:04 +0100

    Add second letter of second chapter

--- lastsanta/gifts.py
+++ lastsanta/gifts.py
@@ -4,6 +4,7 @@ from .logic import JointConstraint, ItemSetConstraint, ItemsNumberConstraint
 # Some useful constants for part names
 LIGHTBULB = 'lightbulb'
 BASE1 = 'base1'
+BASES = 'base1', 'base2', 'base3'
 WHEEL = 'wheel'
 DRAGON_HEAD = 'dragon_head'
 HUMAN_PARTS = 'arm', 'leg', 'eye'
@@ -33,5 +34,6 @@ gifts = {
         JointConstraint(
             ItemSetConstraint(1, DRAGON_HEAD),
             ItemSetConstraint(2, *HUMAN_PARTS)
-        )
+        ),
+    'carpet': ItemSetConstraint(3, *BASES)
 }
